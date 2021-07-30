b017305338cf26d1729b6993df148f714155d7b6
Omar Baig <omarbaig1998@gmail.com>
2021-07-30 14:39:32 -0500

    created addLast and print methods to LinkedList.h

--- Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/LinkedList.h
+++ Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/LinkedList.h
@@ -1,27 +1,71 @@
 #pragma once
+#include <iostream>
 
 template <typename T>
 struct Node {
 public:
-	T item;
+	T     item;
 	Node* next;
 };
 
 template <typename T>
 class LinkedList {
 private:
-	Node* Head;
-	Node* Tail;
+	Node<T>* Head;
+	Node<T>* Tail;
 
 	int _length;
 
 public:
 	LinkedList();
-
+	void addLast(T item);
+	void print();
 
 };
 
 template <typename T>
 LinkedList<T>::LinkedList() {
 	_length = 0;
+
+	Head = new Node<T>();
+	Tail = new Node<T>();
+}
+
+template <typename T>
+void LinkedList<T>::addLast(T item) {
+	Node<T>* newNode;
+	newNode = new Node<T>();
+	newNode->item = item;
+	newNode->next = NULL;
+
+	Node<T>* currNode = new Node<T>();
+
+	currNode = Head;
+
+	while (currNode->next != NULL) {
+		currNode = currNode->next;
+	}
+
+	currNode->next = newNode;
+
+	if (_length == 0) {
+		Head = newNode;
+		Tail = newNode;
+	}
+	else{
+		Tail = newNode;
+	}
+
+	_length++;
+}
+
+template <typename T>
+void LinkedList<T>::print() {
+	Node<T>* currNode = new Node<T>();
+	currNode = Head;
+
+	while (currNode != NULL) {
+		std::cout << currNode->item << std::endl;
+		currNode = currNode->next;
+	}
 }
