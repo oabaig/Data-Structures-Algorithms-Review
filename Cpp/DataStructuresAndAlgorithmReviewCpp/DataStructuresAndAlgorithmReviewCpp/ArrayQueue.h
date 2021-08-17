0fffd4734d249f750467bdbb9b9af30a3d6e5ae2
Omar Baig <omarbaig1998@gmail.com>
2021-08-16 20:49:02 -0500

    implemented enqueue method to ArrayQueue.h

--- Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/ArrayQueue.h
+++ Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/ArrayQueue.h
@@ -6,9 +6,11 @@ const int MAXSIZE = 100;
 template <typename T>
 class ArrayQueue {
 private:
-	T* _queue;
-	int _front;
-	int _back;
+	bool _isEmpty;
+	bool _isFull;
+	int  _front;
+	int  _back;
+	T*   _queue;
 
 public:
 	// constructors
@@ -18,6 +20,11 @@ public:
 	~ArrayQueue();
 
 	// methods
+	bool enqueue(T item);
+	bool dequeue();
+	bool peek();
+	bool isEmpty();
+	bool isFull();
 };
 
 // constructors
@@ -25,11 +32,31 @@ template <typename T>
 ArrayQueue<T>::ArrayQueue() {
 	_front = 0;
 	_back = 0;
+	_isEmpty = true;
+	_isFull = false;
 
 	_queue = new T[MAXSIZE];
 }
 
+// destructor
 template <typename T>
 ArrayQueue<T>::~ArrayQueue() {
 	delete _queue;
 }
+
+// methods
+template <typename T>
+bool ArrayQueue<T>::enqueue(T item) {
+	if (!_isFull) {
+		_isEmpty = false;
+
+		_queue[_back] = item;
+
+		_back++;
+
+		return true;
+	}
+
+	return false;
+}
+
