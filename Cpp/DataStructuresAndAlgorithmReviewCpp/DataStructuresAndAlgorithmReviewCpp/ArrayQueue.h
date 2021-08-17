a575d207454c6619be6e36533eb31cea6b88c6ad
Omar Baig <omarbaig1998@gmail.com>
2021-08-16 20:44:05 -0500

    implemented constructor and destructor for ArrayQueue.h

--- Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/ArrayQueue.h
+++ Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/ArrayQueue.h
@@ -1,8 +1,35 @@
 #pragma once
 #include <iostream>
 
+const int MAXSIZE = 100;
+
 template <typename T>
 class ArrayQueue {
-	private
+private:
+	T* _queue;
+	int _front;
+	int _back;
 
+public:
+	// constructors
+	ArrayQueue();
+
+	// destructors
+	~ArrayQueue();
+
+	// methods
 };
+
+// constructors
+template <typename T>
+ArrayQueue<T>::ArrayQueue() {
+	_front = 0;
+	_back = 0;
+
+	_queue = new T[MAXSIZE];
+}
+
+template <typename T>
+ArrayQueue<T>::~ArrayQueue() {
+	delete _queue;
+}
