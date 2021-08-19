0420bb388fd92adf70e928838435b191381a48bb
Omar Baig <omarbaig1998@gmail.com>
2021-08-19 17:51:54 -0500

    fixed enqueue and print and implemented dequeue to ArrayQueue.h

--- Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/ArrayQueue.h
+++ Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/ArrayQueue.h
@@ -1,27 +1,30 @@
 #pragma once
 #include <iostream>
 
-const int MAXSIZE = 100;
-
 template <typename T>
 class ArrayQueue {
 private:
 	bool _isEmpty;
 	bool _isFull;
 	int  _front;
-	int  _back;
+	int  _back; 
+	int  _maxSize;
+	int  _count;
 	T*   _queue;
 
 public:
+	// getters and setters
+	int getLength() const { return _count; }
+	
 	// constructors
-	ArrayQueue();
+	ArrayQueue(int size);
 
 	// destructors
 	~ArrayQueue();
 
 	// methods
 	bool enqueue(T item);
-	bool dequeue();
+	T dequeue();
 	bool peek();
 	bool isEmpty();
 	bool isFull();
@@ -30,13 +33,15 @@ public:
 
 // constructors
 template <typename T>
-ArrayQueue<T>::ArrayQueue() {
+ArrayQueue<T>::ArrayQueue(int size) {
 	_front = 0;
 	_back = 0;
 	_isEmpty = true;
 	_isFull = false;
+	_maxSize = size;
+	_count = 0;
 
-	_queue = new T[MAXSIZE];
+	_queue = new T[_maxSize];
 }
 
 // destructor
@@ -48,34 +53,29 @@ ArrayQueue<T>::~ArrayQueue() {
 // methods
 template <typename T>
 void ArrayQueue<T>::print() {
-	for (int i = _front; i < _back; i++) {
-		if (i % 10 == 0 && i != _front) {
-			std::cout << std::endl << " " << _queue[i] << ", ";
-			continue;
-		}
-
-		if (i == _front) {
-			std::cout << "[" << _queue[i] << ", ";
-			continue;
-		}
-
-		if (i == _back - 1) {
-			std::cout << _queue[i] << "]" << std::endl;
-			continue;
-		}
-
-		std::cout << _queue[i] << ", ";
+	int start = _front;
+	for (int i = 0; i < _count; i++) {
+		std::cout << _queue[start] << " ";
+		start = (start + 1) % _maxSize;
 	}
+	std::cout << std::endl;
 }
 
 template <typename T>
 bool ArrayQueue<T>::enqueue(T item) {
 	if (!_isFull) {
-		_isEmpty = false;
 
 		_queue[_back] = item;
+		_back = (_back + 1) % _maxSize;
+		_count++;
 
-		_back++;
+
+		if (_count == _maxSize) {
+			_isFull = true;
+		}
+
+		_isEmpty = false;
+
 
 		return true;
 	}
@@ -83,3 +83,23 @@ bool ArrayQueue<T>::enqueue(T item) {
 	return false;
 }
 
+template <typename T>
+T ArrayQueue<T>::dequeue() {
+	if (!_isEmpty) {
+		_isFull = false;
+
+		T item = _queue[_front];
+
+		_front = (_front + 1) % _maxSize;
+
+		_count--;
+
+		if (_count == 0) {
+			_isEmpty = true;
+		}
+
+		return item;
+	}
+
+	return NULL;
+}
