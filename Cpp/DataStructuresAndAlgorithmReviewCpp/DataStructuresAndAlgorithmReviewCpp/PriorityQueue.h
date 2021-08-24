cc90851dc720a6e6994ed1b813f9dbbe306aa19b
Omar Baig <omarbaig1998@gmail.com>
2021-08-24 10:52:31 -0500

    implemented enqueue for priorityqueue.h

--- Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/PriorityQueue.h
+++ Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/PriorityQueue.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 class PriorityQueue {
 private:
@@ -8,6 +9,9 @@ private:
 	bool _isEmpty;
 	bool _isFull;
 
+	int _front;
+	int _back;
+
 public:
 	// getters and setters
 	bool isEmpty() const { return _isEmpty; }
@@ -15,14 +19,76 @@ public:
 
 	// constructors
 	PriorityQueue(int size);
+
+	// methods
+	bool enqueue(int item);
+	int  dequeue();
+	void print();
 };
 
 PriorityQueue::PriorityQueue(int size) {
-	 _size = 0;
-	 _maxSize = size;
-	 _isEmpty = true;
-	 _isFull = false;
+	_size = 0;
+	_maxSize = size;
+	_isEmpty = true;
+	_isFull = false;
+
+	_front = 0;
+	_back = 0;
 	
 	_queue = new int[size];
 }
 
+bool PriorityQueue::enqueue(int item) {
+	if (!_isFull) {
+
+		int index = _front;
+		bool foundLoc = false;
+		for (int i = 0; i < _size; i++) {
+			if (item < _queue[index]) {
+				int tempArraySize = _size - (index);
+
+				int* tempArray = new int[tempArraySize];
+				int tempIndex = index;
+				for (int k = 0; k < tempArraySize; k++) {
+					tempArray[k] = _queue[tempIndex];
+
+					tempIndex = (tempIndex + 1) % _maxSize;
+				}
+
+				_queue[index] = item;
+
+				int newIndex = (index + 1) % _maxSize;
+				for (int j = 0; j < tempArraySize; j++) {
+					_queue[newIndex] = tempArray[j];
+
+					newIndex = (newIndex + 1) % _maxSize;
+				}
+
+				_back = (_back + 1) % _maxSize;
+				foundLoc = true;
+				break;
+			}
+			index = (index + 1) % _maxSize;
+		}
+
+		if (!foundLoc) {
+			_queue[_back] = item;
+			_back = (_back + 1) % _maxSize;
+		}
+
+		_size++;
+		if (_size == _maxSize) {
+			_isFull = true;
+		}
+	}
+	return false;
+}
+
+void PriorityQueue::print() {
+	int index = _front;
+	for (int i = 0; i < _size; i++) {
+		std::cout << _queue[index] << " ";
+		index = (index + 1) % _maxSize;
+	}
+	std::cout << std::endl;
+}
