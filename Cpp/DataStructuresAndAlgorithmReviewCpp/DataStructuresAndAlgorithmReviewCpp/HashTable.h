5948840a1134cb9f62756d17119887021b41020b
Omar <48194266+oabaig@users.noreply.github.com>
2021-09-10 16:24:33 -0500

    updated hashtable.h to use the standard list instead of my own list

--- Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/HashTable.h
+++ Cpp/DataStructuresAndAlgorithmReviewCpp/DataStructuresAndAlgorithmReviewCpp/HashTable.h
@@ -1,69 +1,36 @@
 #pragma once
-#include "LinkedList.h"
+#include <list>
 
 class HashTable {
 private:
 	struct Entry {
+	public:
 		int key = 0;
 		std::string value;
 	};
 
-	LinkedList<Entry>* _table;
+	std::list<Entry>* _table;
 	int _maxSize;
-
-	int hash(int key) {
-		return key % _maxSize;
-	}
+	int _size;
 
 public:
-	// constructors
+	HashTable();
 	HashTable(int size);
-
-	// methods
 	void put(int k, std::string v);
-	std::string get(int k);
 };
+
+HashTable::HashTable(){
+	_maxSize = 5;
+	_size = 0;
+	_table = new std::list<Entry>[_maxSize];
+}
 
 HashTable::HashTable(int size) {
 	_maxSize = size;
-
-	_table = new LinkedList<Entry>[_maxSize];
+	_size = 0;
+	_table = new std::list<Entry>[_maxSize];
 }
 
 void HashTable::put(int k, std::string v) {
 
-	int index = hash(k);
-
-	Entry newEntry;
-	newEntry.key = k;
-	newEntry.value = v;
-	/*
-	int i = 0;
-	if (!_table[index].isEmpty()) {
-		for (i; i < _table[index].getLength(); i++) {
-			//Entry retrievedEntry = _table[index].GetKthIndex(i);
-
-			if (retrievedEntry.key == k) {
-				break;
-			}
-		}
-
-
-	}
-
-	_table[index].addAtIndex(i, newEntry);*/
-
-	while (_table[index].getNext().key != k) {
-
-	}
-}
-
-std::string HashTable::get(int k) {
-	int index = hash(k);
-
-	Entry getEntry;
-
-	LinkedList<Entry> getList = _table[index];
-
-	//return getList.GetKthIndex(k).value;
 }
